d9f47531c655c36149c19f958617466536392ab4
Glen Adamson <glen.adamson@terraquest.co.uk>
2021-06-27 08:26:28 +0100

    adding a new expense form and looking at useState as an example

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,23 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../Card";
+import { useState } from "react";
 
 const ExpenseItem = ({ title, amount, date }) => {
+  const [newTitle, setNewTitle] = useState(title);
+
+  const ClickHandler = () => {
+    setNewTitle("glen");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{newTitle}</h2>
         <div className="expense-item__price">£{amount}</div>
       </div>
+      <button onClick={ClickHandler}>Change Title</button>
     </Card>
   );
 };
